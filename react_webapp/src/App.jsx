8add9925c8833ec58df623954f8f37c7923d4a22
Stickman230 <maxime.reynaud230@gmail.com>
2024-04-17 12:29:48 +0200

    Added skeleton for react_webapp

---
+++ react_webapp/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='main_container'>
+      <p>Hello World</p>
+      <div className='container1'>
+        <p>container1</p>
+      </div>
+      <div className='container2'>
+        <p>container2</p>
+      </div>
+      <div className='container3'>
+        <p>container3</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
